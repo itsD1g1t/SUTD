d1d65b9654cb5b32b04c678d01f639c7c9b45102
itsD1g1t <164067391+itsD1g1t@users.noreply.github.com>
2024-04-14 11:26:22 +0800

    Add files via upload

---
+++ FlaskFolder2/setup.py
@@ -0,0 +1,12 @@
+# How to Set up environment to run program:
+
+# type the following in terminal
+# . .venv/bin/activate
+# export FLASK_APP=app
+# export FLASK_ENV=development
+# flask run --debug
+
+
+# Add “--host=0.0.0.0” to the end of the line above to make it publicly available
+# Add “-p 5001” to change port if 5000 already in use
+
